c40bd72aadfddf27448385eb3f06c34733b32b1a
SkaLe3 <39skale@gmail.com>
2023-12-11 19:16:38 +0200

    Added premake

---
+++ premake.lua
@@ -0,0 +1,37 @@
+project "TinyORM"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**/*.cpp",
+        "src/**/*.h",
+        "src/**/*.hpp"
+	}
+
+	local projectDir = os.getcwd()
+	includedirs
+	{
+		projectDir
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
